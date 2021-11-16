b89db36f7ee15ab5c38a16c90a57b43dc3761536
Naufal Sayyid Furqoon <naufalsayyid1@gmail.com>
2021-11-16 12:43:37 +0700

    change image source

---
+++ js/styling.js
@@ -0,0 +1,6 @@
+function changeSectiontwo(){
+    let section1 = document.getElementById("homePage");
+    section1.className = "hidden";
+    let section2 = document.getElementById("aboutMe");
+    section2.className ="show";
+}
